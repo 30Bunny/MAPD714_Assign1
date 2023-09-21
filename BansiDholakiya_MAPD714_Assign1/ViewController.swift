6249395ca14d77cba46771e128ceb702990cfd5a
30Bunny <bansidholakia@gmail.com>
2023-09-21 07:52:24 -0400

    Initial Commit

---
+++ BansiDholakiya_MAPD714_Assign1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BansiDholakiya_MAPD714_Assign1
+//
+//  Created by Bansi Dholakiya on 2023-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
